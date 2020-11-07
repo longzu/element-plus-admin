4f1780db81081a451c56a2b7b6809d0153d47ddf
ntnyq <ntnyq@users.noreply.github.com>
2020-11-07 21:11:05 +0000

    deploy: 5f47045a70faf27847c1fcc7e3c6a78666065e84

--- js/chunk-76e9fd27.f86d1d72.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76e9fd27"],{e97a:function(o,n,p){o.exports={primaryColor:"#095ce5"}}}]);
-//# sourceMappingURL=chunk-76e9fd27.f86d1d72.js.map
