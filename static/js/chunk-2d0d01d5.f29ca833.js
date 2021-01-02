9e92e233019567b07e21ca5c9d8fe66ba0e1abab
ntnyq <ntnyq@users.noreply.github.com>
2021-01-02 21:44:57 +0000

    deploy: b861ddf6299b8ec2e2cac8d43cfea9d53fd93570

---
+++ static/js/chunk-2d0d01d5.f29ca833.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d01d5"],{6731:function(e,n,d){"use strict";d.r(n);var c=d("7a23"),o=Object(c["defineComponent"])({name:"Redirect"});n["default"]=o}}]);
