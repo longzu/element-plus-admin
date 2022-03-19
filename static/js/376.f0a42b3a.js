d9e0142ec63a3e4c54c70a8d7329bad250885d7d
longzu <longzu@users.noreply.github.com>
2022-03-19 14:21:58 +0000

    deploy: 5727cc5f03c8adc0dc861704c846c146a2d80b81

---
+++ static/js/376.f0a42b3a.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkelement_plus_admin"]=self["webpackChunkelement_plus_admin"]||[]).push([[376],{4376:function(e,t,r){r.r(t),r.d(t,{default:function(){return p}});var u=r(1831),n=r(7853),a={name:"TheRedirect",setup(){const{route:e,router:t}=(0,n.j)(),{params:r,query:a}=e,{path:s}=r;return t.replace({path:`/${s}`,query:a}),()=>(0,u.h)("div")}};const s=a;var p=s}}]);
--- static/js/787.9cb326f1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkelement_plus_admin"]=self["webpackChunkelement_plus_admin"]||[]).push([[787],{8787:function(e,t,r){r.r(t),r.d(t,{default:function(){return p}});var u=r(2970),n=r(4627),a={name:"TheRedirect",setup(){const{route:e,router:t}=(0,n.j)(),{params:r,query:a}=e,{path:s}=r;return t.replace({path:`/${s}`,query:a}),()=>(0,u.h)("div")}};const s=a;var p=s}}]);
